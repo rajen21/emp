2aa187ead9046abb47aea1280ad055d9b38f35e0
Rajan chavda <www.chavdarajan@gmail.com>
2024-10-13 18:38:56 +0530

    token changes

--- src/utils/Api.ts
+++ src/utils/Api.ts
@@ -25,8 +25,6 @@ axiosInstance.interceptors.request.use(
 axiosInstance.interceptors.response.use(
   (response: AxiosResponse) => {
     const token = {authToken: response.headers["authorization"]}
-    console.log("check auttoke", token, response.headers["authorization"]);
-    debugger;
     
     if (token.authToken) {
       localStorage.setItem("emp-token", JSON.stringify(token));
